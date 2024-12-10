c70d954348bcc4dd7d3054c9bb04d35ad328c09b
Frodewald Grimbert <67095670+frodewald@users.noreply.github.com>
2024-12-10 21:16:11 +0700

    Delete quiz-server directory

--- quiz-server/app/routes/record.route.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-  const records = require('../controllers/record.controller')
-  const router = require('express').Router()
-
-  router.post('/record/post', records.postRecords)
-  router.get('/record/getAllRecords', records.getRecordsByHighestScore);
-
-  app.use('/api', router)
-}
