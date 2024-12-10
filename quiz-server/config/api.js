ea0021f30ab80f8788697466098d31779b1988af
Frodewald <frodewaldgrimbert777@gmail.com>
2024-12-10 21:07:40 +0700

    First commit for quiz-client

--- quiz-server/config/api.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = 'http://localhost:8000';
-const FE_BASE_URL = 'http://localhost:3000';
-
-module.exports = {
-  BASE_URL,
-  FE_BASE_URL
-};
--- quiz-server/config/db.config.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    url: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`
-}
