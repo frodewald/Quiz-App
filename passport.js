ddf60934cb4a5b27339338992a0deb841b71ed78
Frodewald Grimbert <67095670+frodewald@users.noreply.github.com>
2024-12-11 10:44:36 +0700

    Update passport.js

--- passport.js
+++ passport.js
@@ -25,7 +25,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://quiz-app-c871.onrender.com/auth/google/callback',
+      callbackURL: '${BASE_URL}/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
