4d3e4522e416122744d0cdbda65ebdb899fd9cb1
Frodewald Grimbert <67095670+frodewald@users.noreply.github.com>
2024-12-11 10:42:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
   session({
     secret: process.env.SECRET_KEY,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     store: MongoStore.create({
       mongoUrl: db.url,
       collectionName: 'sessions',
