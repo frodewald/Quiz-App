113bf2fcbd166bccaebf4dbb8e0c978a9e4d57e4
Frodewald <frodewaldgrimbert777@gmail.com>
2024-12-12 20:53:21 +0700

    perubahan base_url

--- quiz-server/config/api.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = 'http://localhost:8000';
-// const BASE_URL = 'https://quiz-app-c871.onrender.com';
-const FE_BASE_URL = 'http://localhost:3000';
-
-module.exports = {
-  BASE_URL,
-  FE_BASE_URL
-};
