1d89a3e9513da3ca0636bc23a25861a8dcb15103
Frodewald Grimbert <67095670+frodewald@users.noreply.github.com>
2024-12-14 00:27:03 +0700

    Update api.js

--- quiz-server/config/api.js
+++ quiz-server/config/api.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://quiz-app-production-da75.up.railway.app';
-const FE_BASE_URL = 'https://quiz-app-client-three.vercel.app';
+const FE_BASE_URL = 'https://quiz-app-client-production.up.railway.app';
 
 module.exports = {
   BASE_URL,
